93fba508edcf2abb85b71b023d6f2965788ed36f
Shannon Dela Pena <sldelapena0505@gmail.com>
2024-10-28 01:19:05 +1000

    added user location to context

--- components/context/UserContext.jsx
+++ components/context/UserContext.jsx
@@ -1,6 +1,7 @@
-// components/context/LocationContext.js
+// components/context/UserContext.js
 import React, { createContext, useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import * as Location from "expo-location";
 
 // Create the context
 export const UserContext = createContext();
@@ -8,6 +9,7 @@ export const UserContext = createContext();
 // Create the provider component
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState("");
+  const [userLocation, setUserLocation] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -25,11 +27,34 @@ export const UserProvider = ({ children }) => {
       }
     };
 
+    // Conditionally start watching the user's location based on project scoring
+    const startWatchingLocation = async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === "granted") {
+        await Location.watchPositionAsync(
+          {
+            accuracy: Location.Accuracy.High,
+            distanceInterval: 10, // Update every 10 meters
+          },
+          (location) => {
+            const userCoordinates = {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            };
+            setUserLocation(userCoordinates);
+          }
+        );
+      } else {
+        console.error("Permission to access location was denied");
+      }
+    };
+
     loadUserData();
+    startWatchingLocation();
   }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser, loading }}>
+    <UserContext.Provider value={{ user, setUser, loading, userLocation }}>
       {children}
     </UserContext.Provider>
   );
