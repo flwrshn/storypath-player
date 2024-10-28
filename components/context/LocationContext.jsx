c0677411777dbea7c1d031e7294a30f03c757e00
Shannon Dela Pena <sldelapena0505@gmail.com>
2024-10-28 18:31:34 +1000

    removed visited locations

--- components/context/LocationContext.jsx
+++ components/context/LocationContext.jsx
@@ -5,15 +5,9 @@ export const LocationContext = createContext();
 
 export const LocationProvider = ({ children }) => {
   const [locations, setLocations] = useState([]);
-  const [visitedLocations, setVisitedLocations] = useState(new Set());
-  const markLocationVisited = (locationId) => {
-    setVisitedLocations((prevVisited) => new Set(prevVisited).add(locationId));
-  };
 
   return (
-    <LocationContext.Provider
-      value={{ locations, setLocations, visitedLocations, markLocationVisited }}
-    >
+    <LocationContext.Provider value={{ locations, setLocations }}>
       {children}
     </LocationContext.Provider>
   );
