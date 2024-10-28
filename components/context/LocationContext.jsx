7c48cf41b69eb422ab69775b3e3cdf1c2cc6112c
Shannon Dela Pena <sldelapena0505@gmail.com>
2024-10-28 18:05:05 +1000

    added visited locations state variable

--- components/context/LocationContext.jsx
+++ components/context/LocationContext.jsx
@@ -1,15 +1,19 @@
 // contexts/LocationContext.js
 import React, { createContext, useState } from "react";
 
-// Create the context
 export const LocationContext = createContext();
 
-// Create the provider component
 export const LocationProvider = ({ children }) => {
   const [locations, setLocations] = useState([]);
+  const [visitedLocations, setVisitedLocations] = useState(new Set());
+  const markLocationVisited = (locationId) => {
+    setVisitedLocations((prevVisited) => new Set(prevVisited).add(locationId));
+  };
 
   return (
-    <LocationContext.Provider value={{ locations, setLocations }}>
+    <LocationContext.Provider
+      value={{ locations, setLocations, visitedLocations, markLocationVisited }}
+    >
       {children}
     </LocationContext.Provider>
   );
