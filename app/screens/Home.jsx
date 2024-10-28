c63015e3179fe4de90432905ba7f37ea2561add6
Shannon Dela Pena <sldelapena0505@gmail.com>
2024-10-28 18:30:56 +1000

    conditionally print username

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -4,8 +4,10 @@ import React, { useContext } from "react";
 import { UserContext } from "@/components/context/UserContext";
 
 const Home = ({}) => {
+  const { user } = useContext(UserContext);
   return (
     <View>
+      <Text>{user ? user : "Guest"}</Text>
       <Text className="font-bold">Home</Text>
     </View>
   );
